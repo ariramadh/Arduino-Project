011d65c9e90842ae7b5e26a04b269bfbcbe55941
ariramadh <ariramadhani24@gmail.com>
2022-10-07 17:25:48 +0700

    Initial setup connection to Thingspeak

---
+++ Simple_Rain_Prediction/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Rvs2"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "Le_m0n@e"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 1885085			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "RB286XRO3X3KU9IL"   // replace XYZ with your channel write API Key
